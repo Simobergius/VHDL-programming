479729fd6545b9537781da87b453109df2922f57
Aleksi-Peltonen <aleksi.peltonen@eng.tamk.fi>
2018-11-13 18:59:04 +0200

    TB updates

--- TB.vhd
+++ TB.vhd
@@ -2,10 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity TestBench is
-end entity TestBench;
+entity SPI_TestBench is
+end entity SPI_TestBench;
 
-architecture top_level of TestBench is
+architecture top_level of SPI_TestBench is
 
 use work.SPI_SLAVE;
 
@@ -13,7 +13,7 @@ signal SlaveIn : std_logic := '0';
 signal SlaveOut : std_logic := '0';
 signal SPICLK : std_logic := '0';
 signal SCLK : std_logic := '0';
-signal ChipE : std_logic := '1';
+signal ChipE : std_logic := '0';
 signal cmd : std_logic_vector (7 downto 0) := B"00001000";
 signal SlaveParallelIn : std_logic_vector (7 downto 0);
 signal SlaveParallelOut : std_logic_vector (7 downto 0);
@@ -23,7 +23,12 @@ begin
 SPI_KELLO: process is 
 begin
 	wait for 1 us;		--1 us = 500kHz
-	SPICLK <= NOT SPICLK; 
+	if ChipE = '0' then
+	SPICLK <= NOT SPICLK;
+	else
+	SPICLK <= '0';
+	end if;
+
 end process SPI_KELLO;
 
 
@@ -34,6 +39,19 @@ begin
 end process SYSTEM_KELLO;
 
 
+SEND_DATA: process is
+variable counter : integer := 0;
+begin
+wait until rising_edge(SPICLK);
+SlaveIn <= cmd(counter);
+counter := counter + 1;
+if counter = 7 then
+counter := 0;
+end if;
+end process;
+
+
+
 Joku : entity SPI_SLAVE
 	port map (
 		SI	=> SlaveIn,
@@ -41,8 +59,8 @@ Joku : entity SPI_SLAVE
 		SCLK  	=> SPICLK,
 		sys_clk => SCLK,
 		ENABLE 	=> ChipE,
-        parallel_data_in => SlaveParallelIn,
-        parallel_data_out => SlaveParallelOut
+        	parallel_data_in => SlaveParallelIn,
+        	parallel_data_out => SlaveParallelOut
 	);
 
 end architecture;
