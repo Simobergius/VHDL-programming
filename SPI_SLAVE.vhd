631ea2f25b9e24b84232eb3463bbe542812c254b
Simo <simo.bergius1@gmail.com>
2018-11-17 20:53:36 +0200

    Updated SPI_SLAVE

--- SPI_SLAVE.vhd
+++ SPI_SLAVE.vhd
@@ -22,10 +22,11 @@ use work.edge_latch;
 
 type commands is (OP_READ, OP_WRITE, OP_READWRITE, OP_READ_OP, OP_NOP);
 
-signal cmd : commands := OP_NOP;
+signal cmd : commands := OP_READ_OP;
 signal read_cmd : std_logic := '1';
 signal enable_triggered : std_logic := '0';
 signal enable_triggered_falling : std_logic := '0';
+signal enable_triggered_rising : std_logic := '0';
 signal counter : integer := 0;
 signal data_in : std_logic_vector(7 downto 0);
 
@@ -33,13 +34,15 @@ signal data_in : std_logic_vector(7 downto 0);
 
 begin
     
-    enable_trigd : process (sys_clk) is
+    process (sys_clk) is
     begin
     if enable_triggered = '1' then
         if ENABLE = '0' then
             enable_triggered_falling <= '1';
+            enable_triggered_rising <= '0';
         else
             enable_triggered_falling <= '0';
+            enable_triggered_rising <= '1';
         end if;
     end if;
     end process;
@@ -47,47 +50,46 @@ begin
     process is
     begin
     wait until rising_edge(SCLK);
-    if enable_triggered_falling = '1' then
-        cmd <= OP_READ_OP;
-    else 
-        cmd <= OP_NOP;
-    end if;
-    case cmd is
-        when OP_READ_OP =>
-            -- Read 8 bits from SI port
-            if counter = 8 then
-                --Stop reading at eigth bit and plop command into signal
-                if data_in = B"00000000" then 
-                    cmd <= OP_NOP;
-                elsif data_in = B"00000001" then
-                    cmd <= OP_READ;
-                elsif data_in = B"00000010" then
-                    cmd <= OP_WRITE;
-                elsif data_in = B"00000011" then
-                    cmd <= OP_READWRITE;
+    if ENABLE = '0' then
+        case cmd is
+            when OP_READ_OP =>
+                -- Read 8 bits from SI port
+                if counter = 8 then
+                    --Stop reading at eigth bit and plop command into signal
+                    if data_in = B"00000000" then 
+                        cmd <= OP_NOP;
+                    elsif data_in = B"00000001" then
+                        cmd <= OP_READ;
+                    elsif data_in = B"00000010" then
+                        cmd <= OP_WRITE;
+                    elsif data_in = B"00000011" then
+                        cmd <= OP_READWRITE;
+                    else
+                        cmd <= OP_NOP;
+                    end if;
                 else
-                    cmd <= OP_NOP;
+                    --Read bit from SI port
+                    data_in(counter) <= SI;
                 end if;
-                read_cmd <= '0';
-            elsif read_cmd = '1' then
-                --Read bit from SI port
-                data_in(counter) <= SI;
-            end if;
-            
-        when OP_READ => 
-            --Read data
-            data_in(counter-8) <= SI;
-        when OP_WRITE => NULL;
-        when OP_READWRITE => 
-            --Read data
-            data_in(counter-8) <= SI;
-        when OP_NOP => NULL;
-    end case;
-    -- increment counter
-    counter <= counter + 1;
-    if counter >= 16 then
+                
+            when OP_READ => 
+                --Read data
+                data_in(counter-8) <= SI;
+            when OP_WRITE => NULL;
+            when OP_READWRITE => 
+                --Read data
+                data_in(counter-8) <= SI;
+            when OP_NOP => NULL;
+        end case;
+        -- increment counter
+        counter <= counter + 1;
+        if counter >= 16 then
+            counter <= 0;
+            parallel_data_out <= data_in;
+        end if;
+    else
         counter <= 0;
-        parallel_data_out <= data_in;
+        cmd <= OP_READ_OP;
     end if;
     end process;
     
