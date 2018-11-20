55125b467eb9a61d50e75aafb866050d126d723a
Aleksi-Peltonen <aleksi.peltonen@eng.tamk.fi>
2018-11-20 16:15:29 +0200

    Hopeful TB update

--- TB.vhd
+++ TB.vhd
@@ -18,7 +18,6 @@ signal cmd : std_logic_vector (7 downto 0) := B"00000000"; --MSB first
 signal SlaveParallelIn : std_logic_vector (7 downto 0) := B"10010001";
 signal SlaveParallelOut : std_logic_vector (7 downto 0);
 signal count_clk : integer := 0;
-signal done : std_logic := '0';
 signal slave_spi_in : std_logic_vector (7 downto 0) := B"01110110";
 signal counter : integer := 0;
 
@@ -38,97 +37,62 @@ end process SPI_KELLO;
 
 SYSTEM_KELLO: process is 
 begin
-    wait for 250 ns;            --500 ns = 1 MHz
+    wait for 500 ns;            --500 ns = 1 MHz
     SCLK <= NOT SCLK;
-    count_clk <= count_clk + 1;
-    if count_clk >= 20 then         
-        if cmd = B"00000000" then
-           cmd <= B"10000000";
-           count_clk <= count_clk - 1;
-        else
-        ChipE <= '0';           --Start operation
-        end if;
-    end if;
-    if counter = 16 AND SPICLK = '0' then
-        ChipE <= '1';
-        count_clk <= 0;
-        --Cycle command to the next one
-        if cmd = B"10000000" then
-            cmd <= B"01000000";
-        elsif cmd = B"01000000" then
-            cmd <= B"11000000";
-        else
-            cmd <= B"00000000";
-        end if;
-    end if;
+    
 end process SYSTEM_KELLO;
 
+ChangeCMD: process is
+begin
+    wait until rising_edge(SCLK);
+    if counter = 0 then
+        wait for 2 us;
+    end if;
+    
+    if counter = 15 OR cmd = B"00000000" then
+        
+        if cmd = B"00000000" then   --If cmd = 0, skip to next cmd
+            cmd <= B"10000000";
+            ChipE <= '1';
+        elsif cmd = B"10000000" then
+            wait for 2 us;
+            cmd <= B"01000000";
+            ChipE <= '1';
+        elsif cmd = B"01000000" then
+            wait for 2 us;
+            cmd <= B"11000000";
+            ChipE <= '1';
+        else
+            wait for 2 us;
+            cmd <= B"00000000";
+            ChipE <= '1';
+        end if;
+    else
+        ChipE <= '0';
+    end if;
+
+end process ChangeCMD;
 
 
 SEND_DATA: process is
 begin
     wait until rising_edge(SPICLK);
-  
     if counter <= 7 then
         SlaveIn <= cmd(counter);
     else
         SlaveIn <= slave_spi_in(counter - 8);
     end if;
- 
+    
+    if counter < 15 then
     counter <= counter + 1;
-
-SEND_DATA: process (SPICLK, ChipE) is
-variable counter : integer := 0;
-variable prev_ChipE : std_logic := '1';
-variable prev_SPICLK : std_logic := '0';
-begin
-    
-    if (prev_ChipE XOR ChipE) = '1' then
-        -- ChipE changed
-        if ChipE = '0' then
-            -- Falling edge of ChipE
-            -- Write first bit of cmd
-            SlaveIn <= cmd(counter);
-            counter := counter + 1;
-        end if;
     end if;
     
-    if (SPICLK XOR prev_SPICLK) = '1' then
-        --SPICLK changed state
-        if SPICLK = '0' then
-            -- SPICLK falling edge
-            -- write next bit of cmd
-            if counter <= 7 then
-                SlaveIn <= cmd(counter);
-            else
-                SlaveIn <= slave_spi_in(counter - 8);
-            end if;
-            counter := counter + 1;
-        end if;
-    end if;
-    prev_ChipE := ChipE;
-    prev_SPICLK := SPICLK;
-    wait until rising_edge(SPICLK);
-    if counter <= 7 then
-        SlaveIn <= cmd(counter);
-    else
-        SlaveIn <= slave_spi_in(counter - 8);
-    end if;
- 
-
-    if counter = 15 then
+    wait until rising_edge(SCLK);
+     if counter = 15 then
+     wait for 2 us;
         counter <= 0;
-        done <= '1';
     end if;
-end process;
-
-process is 
-begin
-    wait until falling_edge(SPICLK);
     
-    if counter = 50 then
-        done <= '1';
-    end if;
 end process;
 
 Joku : entity SPI_SLAVE
