b99a4fa9703ab098bcd250c4a9a7f4ed7de54bbf
Simo <simo.bergius1@gmail.com>
2018-12-06 11:52:20 +0200

    Changed SPI process logic, TB now puts SlaveIn into high state between transfers

--- SPI_SLAVE.vhd
+++ SPI_SLAVE.vhd
@@ -30,111 +30,103 @@ signal data_in : std_logic_vector(7 downto 0);
 
 begin
     
-    process is
+    process (SCLK) is
     variable data_in_local : std_logic_vector (7 downto 0) := B"00000000";
     begin
-    wait until rising_edge(SCLK);
-    debug_sclk <= '1';
-    if ENABLE = '0' then
-        debug_en <= '0';
-        case cmd is
-            when OP_READ_OP =>
-                -- Read 8 cmd bits from SI port
-                -- 
-                if counter < 8 then
-                    --Read bit from SI port
-                    data_in(7 - counter) <= SI;
-                end if;
-                
-                -- We can determine command here, on 8th rising edge
-                -- but cant use signal because it doesnt update until next
-                -- cycle so use local variable
-                if counter = 7 then
-                    data_in_local(7 downto 1) := data_in(7 downto 1);
-                    data_in_local(7 - counter) := SI;
-                    if data_in_local = B"00000000" then 
-                        cmd <= OP_NOP;
-                        debug_state <= '00';
-                    elsif data_in_local = B"00000001" then
-                        cmd <= OP_READ;
-                        debug_state <= '01';
-                    elsif data_in_local = B"00000010" then
-                        cmd <= OP_WRITE;
-                        debug_state <= '10';
-                    elsif data_in_local = B"00000011" then
-                        cmd <= OP_READWRITE;
-                        debug_state <= '11';
-                    else
-                        cmd <= OP_NOP;
-                        debug_state <= '00';
+    debug_sclk <= SCLK;
+    debug_en <= ENABLE;
+    if SCLK = '1' then
+        if ENABLE = '0' then
+            case cmd is
+                when OP_READ_OP =>
+                    -- Read 8 cmd bits from SI port
+                    -- 
+                    if counter < 8 then
+                        --Read bit from SI port
+                        data_in(7 - counter) <= SI;
                     end if;
                     
+                    -- We can determine command here, on 8th rising edge
+                    -- but cant use signal because it doesnt update until next
+                    -- cycle so use local variable
+                    if counter = 7 then
+                        data_in_local(7 downto 1) := data_in(7 downto 1);
+                        data_in_local(7 - counter) := SI;
+                        if data_in_local = B"00000000" then 
+                            cmd <= OP_NOP;
+                            debug_state <= "00";
+                        elsif data_in_local = B"00000001" then
+                            cmd <= OP_READ;
+                            debug_state <= "01";
+                        elsif data_in_local = B"00000010" then
+                            cmd <= OP_WRITE;
+                            debug_state <= "10";
+                        elsif data_in_local = B"00000011" then
+                            cmd <= OP_READWRITE;
+                            debug_state <= B"11";
+                        else
+                            cmd <= OP_NOP;
+                            debug_state <= B"00";
+                        end if;
+                        
+                    end if;
+                    
+                when OP_READ => 
+                    --Read data
+                    if counter < 16 then
+                        data_in(7 - (counter-8)) <= SI;
+                    end if;
+                when OP_WRITE => NULL;
+                when OP_READWRITE => 
+                    --Read data
+                    if counter < 16 then
+                        data_in(7 - (counter-8)) <= SI;
+                    end if;
+                when OP_NOP => NULL;
+            end case;
+            -- increment counter
+            counter <= counter + 1;
+            
+            if counter >= 15 then
+                -- Use data_in_local here because signal updates only after process finishes
+                data_in_local(7 downto 1) := data_in(7 downto 1);
+                data_in_local(7 - (counter - 8)) := SI;
+                counter <= 0;
+                if cmd = OP_READWRITE or cmd = OP_READ then
+                    parallel_data_out <= data_in_local;
                 end if;
-                
-            when OP_READ => 
-                --Read data
-                if counter < 16 then
-                    data_in(7 - (counter-8)) <= SI;
-                end if;
-            when OP_WRITE => NULL;
-            when OP_READWRITE => 
-                --Read data
-                if counter < 16 then
-                    data_in(7 - (counter-8)) <= SI;
-                end if;
-            when OP_NOP => NULL;
-        end case;
-        -- increment counter
-        counter <= counter + 1;
-        
-        if counter >= 15 then
-            -- Use data_in_local here because signal updates only after process finishes
-            data_in_local(7 downto 1) := data_in(7 downto 1);
-            data_in_local(7 - (counter - 8)) := SI;
-            counter <= 0;
-            if cmd = OP_READWRITE or cmd = OP_READ then
-                parallel_data_out <= data_in_local;
+                cmd <= OP_READ_OP;
             end if;
+        else 
             cmd <= OP_READ_OP;
+            counter <= 0;
         end if;
-    else 
-        cmd <= OP_READ_OP;
-        counter <= 0;
-        debug_en <= '1';
-    end if;
-    end process;
-    
-    process is
-    begin
-    wait until falling_edge(SCLK);
-    debug_sclk <= '0';
-    if ENABLE = '0' then
-        debug_en <= '0';
-        case cmd is
-            when OP_READ_OP => 
-                -- Put Slave Out into high impedance
-                SO <= 'Z';
-                
-            when OP_READ => 
-                -- Put Slave Out into high impedance
-                SO <= 'Z';
-                
-            when OP_WRITE => 
-                if counter < 16 then
-                    SO <= parallel_data_in(7 - (counter-8));
-                end if;
-                
-            when OP_READWRITE => 
-                if counter < 16 then
-                    SO <= parallel_data_in(7 - (counter-8));
-                end if;
-                
-            when OP_NOP => 
-                -- Put Slave Out into high impedance
-                SO <= 'Z';
-        end case;
-    elsif
-        debug_en <= '1';
+    elsif SCLK = '0' then
+        if ENABLE = '0' then
+            case cmd is
+                when OP_READ_OP => 
+                    -- Put Slave Out into high impedance
+                    SO <= 'Z';
+                    
+                when OP_READ => 
+                    -- Put Slave Out into high impedance
+                    SO <= 'Z';
+                    
+                when OP_WRITE => 
+                    if counter < 16 then
+                        SO <= parallel_data_in(7 - (counter-8));
+                    end if;
+                    
+                when OP_READWRITE => 
+                    if counter < 16 then
+                        SO <= parallel_data_in(7 - (counter-8));
+                    end if;
+                    
+                when OP_NOP => 
+                    -- Put Slave Out into high impedance
+                    SO <= 'Z';
+            end case;
+        end if;
     end if;
     end process;
 
